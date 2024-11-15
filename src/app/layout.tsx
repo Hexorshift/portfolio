6152036db051a4318f4b0c33a8df87ded1f42477
Hexorshift <hexorshift@gmail.com>
2024-11-15 16:40:37 -0500

    fixed theme color tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Provider } from "@/components/ui/provider";
 import { Lexend } from "next/font/google";
 
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
   title: "Abu Chowdhury - Aspiring Software Engineer",
   description:
     "Hello! My name is Abu Chowdhury, and I am an aspiring software engineer. I enjoy working with various technologies and primarily focus on full-stack development.",
-  themeColor: "#38b6ff",
   twitter: {
     title: "Abu Chowdhury - Aspiring Software Engineer",
     description:
@@ -22,12 +21,16 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     url: "https://abuc.me",
-    siteName: "Abu Chowdhury - Aspiring Software Engineer",
+    siteName: "Abu Chowdhury",
     title: "Abu Chowdhury - Aspiring Software Engineer",
     description:
       "Hello! My name is Abu Chowdhury, and I am an aspiring software engineer. I enjoy working with various technologies and primarily focus on full-stack development.",
     images: "https://i.imghippo.com/files/fl4330aw.png"
   }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#38b6ff"
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
